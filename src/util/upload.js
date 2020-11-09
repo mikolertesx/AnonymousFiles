4cca393ec67070c163b93717a07306584c4ff5ab
Mikolertesx <mgangsal@outlook.com>
2020-11-09 15:02:18 -0600

    Preliminar work for a proper frontend

---
+++ src/util/upload.js
@@ -0,0 +1,15 @@
+const FormData = require("form-data");
+const Axios = require("axios");
+const fs = require("fs");
+const url = "https://api.anonymousfiles.io/";
+
+export const uploadFile = async (fileDir) => {
+  const form = new FormData();
+  form.append("file", fs.createReadStream(fileDir));
+  const request = await Axios.default.post(url, form, {
+    headers: form.getHeaders()
+  });
+  return request.data;
+}
+
+export default uploadFile;
